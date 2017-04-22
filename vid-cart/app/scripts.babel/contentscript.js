ffa11ac970df0aff6b5cea55350cea7d65dda12d
Logan Lasiter <lasiterlogan@gmail.com>
2017-04-22 16:26:42 -0700

    updated, works on hulu.

--- vid-cart/app/scripts.babel/contentscript.js
+++ vid-cart/app/scripts.babel/contentscript.js
@@ -9,7 +9,22 @@ var netflixString = "netflix";
 
 if(thisSite.includes(huluString)) {
 
-    var addMark = '<div class="whatever">+</div>';
+    var addMark = '<div id="addMark" class="whatever">+</div> <style>.whatever{z-index:1;width:400px;height:400px;color:red;background-color:green;}</style>';
+
+    function addChild() {
+        console.log("entered")
+        var div = document.getElementsByClassName('subtray')[0];
+
+        // addMark.addEventListener("click", function closeSelf() {
+        //     document.body.removeChild(div[0]);
+        // }, false);
+
+        console.log(div)
+        div.innerHTML += addMark;
+
+        // document.body.appendChild(div);
+        console.log('ended')
+    }
 
     window.onload = function() {
         console.log("worked!")
@@ -19,42 +34,34 @@ if(thisSite.includes(huluString)) {
 
 
         for(var i = 0; i < titleCards.length; i++){
-            titleCards[i].addEventListener("mouseenter",function( event ){
-                var link = event.target.querySelector('a');
-                var plus = event.target.querySelector('.whatever');
-                if (!plus) {
-                    event.target.innerHTML += addMark  + event.target.innerHTML
-                }
-                // var playLink = hoveredTitleCard.getElementByClassName("playLink");
-                // console.log(playLink);
-            });
+            titleCards[i].addEventListener("mouseenter", addChild);
+
+            // for(var i = 0; i < titleCards.length; i++){
+            //     titleCards[i].addEventListener("mouseexit",function( event ){
+            //         var link = event.target.querySelector('a');
+            //         event.target.innerHTML = event.target.innerHTML.slice(addMark.length);
+            //         // var playLink = hoveredTitleCard.getElementByClassName("playLink");
+            //         // console.log(playLink);
+            //     });
+            // }
         }
 
-        for(var i = 0; i < titleCards.length; i++){
-            titleCards[i].addEventListener("mouseexit",function( event ){
-                var link = event.target.querySelector('a');
-                event.target.innerHTML = event.target.innerHTML.slice(addMark.length);
-                // var playLink = hoveredTitleCard.getElementByClassName("playLink");
-                // console.log(playLink);
-            });
-        }
-    }
-
-    window.onscroll = function(){
-        console.log("worked!")
-        var titleCards = document.getElementsByClassName("subtray");
-
-        console.log(titleCards.length)
-
-        for(var i = 0; i < titleCards.length; i++){
-            titleCards[i].addEventListener("mouseover",function( event ){
-                var link = event.target.querySelector('a');
-                console.log(link)
-                event.target.innerHTML += addMark + event.target.innerHTML
-                // var playLink = hoveredTitleCard.getElementByClassName("playLink");
-                // console.log(playLink);
-            });
-        }
+        // window.onscroll = function(){
+        //     console.log("worked!")
+        //     var titleCards = document.getElementsByClassName("subtray");
+        //
+        //     console.log(titleCards.length)
+        //
+        //     for(var i = 0; i < titleCards.length; i++){
+        //         titleCards[i].addEventListener("mouseover",function( event ){
+        //             var link = event.target.querySelector('a');
+        //             console.log(link)
+        //             event.target.innerHTML += addMark + event.target.innerHTML
+        //             // var playLink = hoveredTitleCard.getElementByClassName("playLink");
+        //             // console.log(playLink);
+        //         });
+        //     }
+        // }
     }
 }
 
