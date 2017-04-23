78ec4fef77fe24dcc3a53145bc2e2d0ec0a875e6
Logan Lasiter <lasiterlogan@gmail.com>
2017-04-22 17:34:45 -0700

    got to work on amazon.

--- vid-cart/app/scripts.babel/contentscript.js
+++ vid-cart/app/scripts.babel/contentscript.js
@@ -5,11 +5,15 @@ console.log('VidCart will be dope af');
 var thisSite = window.location.href;
 var huluString = "hulu";
 var netflixString = "netflix";
+var amazonString = "amazon"
+
+var addMark = '<div id="addMark" class="whatever">hello world</div> <style>.whatever{margin-top:-100px;display:inline-block;z-index:1;width:100px;height:100px;color:white;background-color:green;position:relative;}</style>';
+
 
 
 if(thisSite.includes(huluString)) {
 
-    var addMark = '<div id="addMark" class="whatever">+</div> <style>.whatever{z-index:1;width:400px;height:400px;color:red;background-color:green;}</style>';
+    var addMark = '<div id="addMark" class="whatever">hello world</div> <style>.whatever{margin-top:-100px;display:inline-block;z-index:1;width:100px;height:100px;color:white;background-color:green;position:relative;}</style>';
 
     function addChild() {
         console.log("entered")
@@ -20,11 +24,14 @@ if(thisSite.includes(huluString)) {
         // }, false);
 
         console.log(div)
+        // var firstDiv = div.innerHTML;
         div.innerHTML += addMark;
+        // div.appendChild(addMark);
 
         // document.body.appendChild(div);
-        console.log('ended')
+        // console.log(firstDiv)
     }
+
 
     window.onload = function() {
         console.log("worked!")
@@ -62,6 +69,47 @@ if(thisSite.includes(huluString)) {
         //         });
         //     }
         // }
+    }
+}
+
+if(thisSite.includes(amazonString)) {
+
+    function addChild() {
+        console.log("entered")
+        var div = document.getElementsByClassName('dv-shelf-item')[0];
+
+        // addMark.addEventListener("click", function closeSelf() {
+        //     document.body.removeChild(div[0]);
+        // }, false);
+
+        console.log(div)
+        // var firstDiv = div.innerHTML;
+        div.innerHTML += addMark;
+        // div.appendChild(addMark);
+
+        // document.body.appendChild(div);
+        // console.log(firstDiv)
+    }
+
+    window.onload = function() {
+        console.log("worked!")
+        var titleCards = document.getElementsByClassName("dv-shelf-item");
+
+        console.log(titleCards.length)
+
+
+        for(var i = 0; i < titleCards.length; i++){
+            titleCards[i].addEventListener("mouseover", addChild);
+
+            // for(var i = 0; i < titleCards.length; i++){
+            //     titleCards[i].addEventListener("mouseexit",function( event ){
+            //         var link = event.target.querySelector('a');
+            //         event.target.innerHTML = event.target.innerHTML.slice(addMark.length);
+            //         // var playLink = hoveredTitleCard.getElementByClassName("playLink");
+            //         // console.log(playLink);
+            //     });
+            // }
+        }
     }
 }
 
