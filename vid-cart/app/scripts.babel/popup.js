901ba5fac92e3a2d781017f2f772c5386ed70992
Michael Green <me@michaelgreen.net>
2017-05-11 14:36:32 -0700

    show links working

--- vid-cart/app/scripts.babel/popup.js
+++ vid-cart/app/scripts.babel/popup.js
@@ -8,34 +8,22 @@ var console = chrome.extension.getBackgroundPage().console;
 var app = {
 
 	init: function() {
-
 		chrome.storage.sync.get({shows: []},function(data){
-			console.log('hey')
-		
-			// Find a <table> element with id="myTable":
-			var table = document.getElementById("myTable");
-
-			// Create an empty <tr> element and add it to the 1st position of the table:
-			var i;
-			var len = data.shows.length;
-			for(i = 0; i < len; i++){
-
-				var str = '<div class="showDiv">' + data.shows[i].name + '</div></br>';
-				var thisLink = data.shows[i].link;
-				document.body.innerHTML += str;
-				var theseShows = document.getElementsByClassName('showDiv');
-
-				console.log(data.shows[i].link);
-				
-				
-				theseShows[i].onclick = function() {
-					chrome.tabs.create({active: true, url: thisLink});
-					console.log('stuffs')
-				};
-				// cell2.innerHTML = data.shows[i].link;
+			for(var i = 0; i < data.shows.length; i++){
+        var show = data.shows[i];
+        /* Each show has an attribute containing a link to the show. */
+				document.body.innerHTML += '<div data-show="' + show.link + '" class="showDiv">' + show.name + '</div></br>';
 			}
-		});
 
+      var openShow =  function(){
+        chrome.tabs.create({active: true, url: this.getAttribute("data-show")});
+      };
+
+      var showDivs = document.getElementsByClassName("showDiv");
+      for (var i = 0; i < showDivs.length; i++) {
+          showDivs[i].addEventListener('click', openShow, false);
+      }
+		});
 	}
 
 }
